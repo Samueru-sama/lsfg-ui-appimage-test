a7315e746c624482f67b572985b4017fb8a7f5d7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-23 05:57:58 -0400

    Create lsfg-ui-appimage.sh

---
+++ lsfg-ui-appimage.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+set -eux
+
+ARCH=$(uname -m)
+URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
+UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+
+# build the thing
+git clone https://github.com/Caliel666/lsfg-vk-ui.git ./lsfg-ui && (
+	cd ./lsfg-ui
+	cargo build --release
+)
+
+VERSION=$(awk -F'=|"' '/^version/{print $3}' ./lsfg-ui/Cargo.toml)
+
+# deploy dependencies
+mkdir -p ./AppDir/shared/bin
+cp -v ./lsfg-ui/resources/icons/lsfg-vk.png ./AppDir/lsfg-ui.png
+cp -v ./lsfg-ui/resources/icons/lsfg-vk.png ./AppDir/.DirIcon
+mv -v ./lsfg-ui/target/release/lsfg-vk-ui   ./AppDir/shared/bin && (
+	cd ./AppDir
+	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+	chmod +x ./sharun-aio
+	xvfb-run -a ./sharun-aio l -p -v -e -s -k  \
+		./shared/bin/lsfg-vk-ui            \
+		/usr/lib/gdk-pixbuf-*/*/loaders/*  \
+		/usr/lib/gio/modules/libdconfsettings.so
+	rm -f ./sharun-aio
+	ln ./sharun ./AppRun
+	./sharun -g
+)
+
+# turn AppDIr into appimage with uruntime
+cd ..
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
+chmod +x ./uruntime*
+
+# Add udpate info to runtime
+echo "Adding update information \"$UPINFO\" to runtime..."
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
+
+echo "Generating AppImage..."
+./uruntime --appimage-mkdwarfs -f \
+	--set-owner 0 --set-group 0 \
+	--no-history --no-create-timestamp \
+	--compression zstd:level=22 -S26 -B8 \
+	--header uruntime-lite \
+	-i ./AppDir -o ./lsfg-ui-"$VERSION"-anylinux-"$ARCH".AppImage
+
+echo "Generating zsync file..."
+zsyncmake *.AppImage -u *.AppImage
+
+echo "All Done!"
